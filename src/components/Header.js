b8da74ed178049c6c632d486c8ecaa496b850a32
prakash <raj505926@gmail.com>
2025-03-01 11:29:53 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,11 +8,11 @@ export default function Header() {
         <a className="font-bold text-black" href="#">Prakash</a>
         <nav className="hidden md:block">
             <ul className="flex text-white ">
-                <li><a href="/">Home</a></li>
-                <li><a href="/#about">About</a></li>
-                <li><a href="/#projects">Projects</a></li>
-                <li><a href="/#resume">Resume</a></li>
-                <li><a href="/#contact">Contact</a></li>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#resume">Resume</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul> 
         </nav>
         {toggleMenu && <nav className="block md:hidden ">
