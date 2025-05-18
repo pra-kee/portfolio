7e5b6c9951aa4821cffddfe76ff3faa7fedea9a6
pra-kee <raj505926@gmail.com>
2025-05-18 09:37:57 +0530

    update pdf path

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -34,7 +34,7 @@ export default function Hero() {
 
                 <div className='flex flex-col justify-center text-black'>
                     <h1 className='text-4xl border-b-4 border-primary mb-5 w-[140px] font-bold'></h1>
-                    <p className='pb-5'><a className='bg-neutral-400 hover:bg-neutral-500 px-5 py-2 font-bold border-2 border-white rounded' href="/Prakash_B.SC.CS_2024_CGPA-6.7.pdf" download="Prakash_Resume.pdf"> Download CV  📄</a> 
+                    <p className='pb-5'><a className='bg-neutral-400 hover:bg-neutral-500 px-5 py-2 font-bold border-2 border-white rounded' href="/files/Prakash_B.SC.CS_2024_CGPA-6.7.pdf" download="Prakash_Resume.pdf"> Download CV  📄</a> 
                     </p>
             </div>
 
